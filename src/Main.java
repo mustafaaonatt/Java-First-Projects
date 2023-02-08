d5830c798de3f40fc8e3e099d9feb08f2e6c72d6
mustafaaonatt <monat9494@gmail.com>
2023-02-07 20:45:05 -0500

    This my first  commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Clarusway Team!");
+        System.out.println("I created first java project and pushed GitHub");
     }
 }
